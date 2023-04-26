993a3955cd94f2badc6546f2ef4ac7c069b19a5c
Nik <90584544+NikTern@users.noreply.github.com>
2023-04-26 16:00:40 +1000

    refactored server.js file

--- server/server.js
+++ server/server.js
@@ -3,6 +3,19 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//import apolloserver class from apolloserver express package
+const { ApolloServer } = require('apollo-server-express'); 
+
+// Import the schema and resolvers for the GraphQL API
+const { typeDefs, resolvers } = require('./schemas');
+
+// Create a new instance of ApolloServer with the schema and resolvers
+const server = new ApolloServer({ 
+  typeDefs,
+  resolvers
+});
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,8 +27,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start(); // Start the ApolloServer
+  server.applyMiddleware({ app }); // Add the ApolloServer middleware to the Express app
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+  
+};
+
+startApolloServer(typeDefs, resolvers); 
